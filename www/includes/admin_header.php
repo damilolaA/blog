793aaeea05fdafadca25e2e6e1ec48e635c234e5
dammy <adiodamilola@yahoo.com>
2017-05-04 18:19:15 +0100

    include title variable

--- www/includes/admin_header.php
+++ www/includes/admin_header.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Register</title>
+	<title><?php echo $title; ?></title>
 	<link rel="stylesheet" type="text/css" href="styles/styles.css">
 </head>
 <body>
 	<section>
 		<div class="mast">
 			<h1>SWAP<span> BLOG</span></h1>
+			<nav>
+				<ul class="clearfix">
+					<li><a href="admin_home.php" class="selected">add posts</a></li>
+					<li><a href="view_post.php">view posts</a></li>
+					<li><a href="#">logout</a></li>
+				</ul>
+			</nav>
 		</div>
 	</section>
 	
