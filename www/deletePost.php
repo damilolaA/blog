c5f3e48ae37ab485d21698699dc7a0f18f40aa0a
dammy <adiodamilola@yahoo.com>
2017-05-04 21:27:42 +0100

    call deletePost function

---
+++ www/deletePost.php
@@ -0,0 +1,27 @@
+<?php
+
+	session_start();
+
+	include 'includes/functions.php';
+
+	checkLogin();
+
+	include 'includes/db.php';
+
+	if(isset($_GET['post_id'])) {
+		$postID = $_GET['post_id'];
+	}
+
+	deletePost($conn, $postID);
+
+	redirect('view_post.php', "");
+
+
+
+
+
+
+
+
+
+?>
