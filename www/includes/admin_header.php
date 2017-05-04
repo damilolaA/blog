d62246abb511bc649aef5c45eff070837a121472
dammy <adiodamilola@yahoo.com>
2017-05-04 22:02:36 +0100

    add current navigation function

--- www/includes/admin_header.php
+++ www/includes/admin_header.php
@@ -10,8 +10,10 @@
 			<h1>SWAP<span> BLOG</span></h1>
 			<nav>
 				<ul class="clearfix">
-					<li><a href="admin_home.php" class="selected">add posts</a></li>
-					<li><a href="view_post.php">view posts</a></li>
+					<li><a href="admin_home.php" <?php curNav('admin_home.php'); ?> >add posts</a></li>
+					<li><a href="view_post.php" <?php curNav('view_post.php'); ?> >view posts</a></li>
+					<li><a href="admin_login.php" <?php curNav('admin_login.php'); ?> >login</a></li>
+					<li><a href="register.php" <?php curNav('register.php'); ?>>register</a></li>
 					<li><a href="#">logout</a></li>
 				</ul>
 			</nav>
