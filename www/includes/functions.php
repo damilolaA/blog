7e7b633ff4cc319814cebf23bcb292311c61fc44
dammy <adiodamilola@yahoo.com>
2017-05-04 12:45:56 +0100

    add adminRegister function

--- www/includes/functions.php
+++ www/includes/functions.php
@@ -8,6 +8,21 @@
 	}
 
 
+	function adminRegister($dbconn, $input) {
+
+		$stmt = $dbconn->prepare("INSERT INTO admin(fname, lname, email, hash) VALUES(:f, :l, :e, :h)");
+
+		$data = [
+				 ':f' => $input['fname'],
+				 ':l' => $input['lname'],
+				 ':e' => $input['email'],
+				 ':h' => $input['password']
+				];
+				
+		$stmt->execute($data);
+	}
+
+
 
 
 ?>
