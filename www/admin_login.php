389381bd1bec57a329841045c3c4b92337150d2d
dammy <adiodamilola@yahoo.com>
2017-05-04 13:10:22 +0100

    start login validation

---
+++ www/admin_login.php
@@ -0,0 +1,57 @@
+<?php
+	
+	include 'includes/admin_header.php';
+
+	include 'includes/db.php';
+
+	include 'includes/functions.php';
+
+	$errors = [];
+
+	if(array_key_exists('login', $_POST)) {
+
+		if(empty($_POST['email'])) {
+			$errors['email'] = "Please enter email address";
+		}
+
+		if(empty($_POST['password'])) {
+			$errors['password'] = "Please enter password";
+		}
+
+		if(empty($errors)) {
+
+
+		}
+
+	}
+
+?>
+	
+	<div class="wrapper">
+		<h1 id="register-label">Admin Login</h1>
+		<hr>
+		<form id="register"  action ="admin_login.php" method ="POST">
+
+			<div>
+				<?php displayError($errors, 'email'); ?>
+				<label>email:</label>
+				<input type="text" name="email" placeholder="email">
+			</div>
+
+			<div>
+				<?php displayError($errors, 'password'); ?>
+				<label>password:</label>
+				<input type="password" name="password" placeholder="password">
+			</div>
+
+			<input type="submit" name="login" value="login">
+		</form>
+
+		<h4 class="jumpto">Don't have an account? <a href="register.php">register</a></h4>
+	</div>
+
+<?php
+
+	include 'includes/admin_footer.php';
+
+?>
