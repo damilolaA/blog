8942386beb27ca2554fa6f2f93b40939075e2358
dammy <adiodamilola@yahoo.com>
2017-05-05 00:12:43 +0100

    add index footer

---
+++ www/includes/index_footer.php
@@ -0,0 +1,7 @@
+
+    <footer class="blog-footer">
+      <p>Blog built for <a href="https://swapspacesystems.com">SwapSpace</a> by <a href="https://twitter.com/justdamilolo">@justdamilolo</a>.</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
