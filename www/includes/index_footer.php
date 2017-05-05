53be332244676e465728efeb5c22587eb32def55
dammy <adiodamilola@yahoo.com>
2017-05-05 21:58:28 +0100

    edit swap space url

--- www/includes/index_footer.php
+++ www/includes/index_footer.php
@@ -1,6 +1,6 @@
 
     <footer class="blog-footer">
-      <p>Blog built for <a href="https://swapspacesystems.com">SwapSpace</a> by <a href="https://twitter.com/justdamilolo">@justdamilolo</a>.</p>
+      <p>Blog built for <a href="http://www.swapspacesystems.com/">SwapSpace</a> by <a href="https://twitter.com/justdamilolo">@justdamilolo</a>.</p>
       <p>
         <a href="#">Back to top</a>
       </p>
